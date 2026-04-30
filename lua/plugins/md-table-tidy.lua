a1b46e17201cd07c21266b55d0b54dd3cd09c087
sigcyc <simonchen1992@gmail.com>
2026-04-30 17:02:03 -0400

    feat: install outline and table-tidy

---
+++ lua/plugins/md-table-tidy.lua
@@ -0,0 +1,5 @@
+return {
+  "timantipov/md-table-tidy.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {},
+}
---
+++ lua/plugins/outline.lua
@@ -0,0 +1,8 @@
+return {
+  "hedyhli/outline.nvim",
+  cmd = { "Outline", "OutlineOpen" },
+  keys = {
+    { "<D-g>", "<cmd>Outline<CR>", desc = "Toggle Outline" },
+  },
+  opts = {},
+}
